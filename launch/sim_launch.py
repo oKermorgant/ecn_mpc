12e50c560558b0f20a11937ef0f83f19904dd517
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-07-01 15:53:48 +0200

    do_mpc wrapper

---
+++ launch/sim_launch.py
@@ -0,0 +1,30 @@
+from simple_launch import SimpleLauncher
+from nav2_common.launch import RewrittenYaml
+
+
+def generate_launch_description():
+
+    sl = SimpleLauncher()
+
+    sl.include('zoe', 'sim_ecn_launch.py', launch_arguments={'display': False, 'rviz': False})
+    sl.rviz(sl.find('ecn_mpc', 'zoe.rviz'))
+
+    nav2_params = sl.find('ecn_mpc', 'nav2.yaml')
+    configured_params = RewrittenYaml(
+                            source_file=nav2_params,
+                            root_key='zoe',
+                            param_rewrites = {},
+                            convert_types=True)
+    remappings = {'/zoe/map': '/map', 'map': '/map', '/scan': 'scan', 'plan': 'plan_raw'}
+
+    with sl.group(ns = 'zoe'):
+        sl.node('nav2_planner', 'planner_server', parameters = [configured_params],
+                remappings = remappings)
+        sl.node('nav2_lifecycle_manager','lifecycle_manager',name='lifecycle_manager',
+                output='screen',
+                parameters=[{'autostart': True,
+                            'node_names': ['planner_server']}])
+
+        sl.node('ecn_mpc', 'plan2traj')
+
+    return sl.launch_description()
